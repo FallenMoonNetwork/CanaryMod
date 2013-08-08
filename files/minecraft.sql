0744cd93e54aebe7414c66c91ee9769c9fdb5c26
Willem Mulder <willemmaster@hotmail.com>
2013-08-08 14:15:37 +0200

    Update item names

--- files/minecraft.sql
+++ files/minecraft.sql
@@ -39,7 +39,7 @@ CREATE TABLE IF NOT EXISTS `groups` (
 
 LOCK TABLES `groups` WRITE;
 /*!40000 ALTER TABLE `groups` DISABLE KEYS */;
-INSERT INTO `groups` VALUES 
+INSERT INTO `groups` VALUES
 (1,'default','f','/help,/sethome,/home,/spawn,/me,/msg,/kit,/playerlist,/warp,/motd,/compass,/tell,/m,/who','',1,0,1,0),
 (2,'vip','a','','default',0,0,1,0),
 (3,'mods','b','/ban,/kick,/item,/tp,/tphere,/s,/i,/give','vip',0,0,1,1),
@@ -91,7 +91,7 @@ CREATE TABLE IF NOT EXISTS `items` (
 --
 LOCK TABLES `items` WRITE;
 /*!40000 ALTER TABLE `items` DISABLE KEYS */;
-INSERT INTO `items` VALUES 
+INSERT INTO `items` VALUES
 ('air',0),('rock',1),('stone',1),('grass',2),('dirt',3),
 ('cobblestone',4),('cobble',4),('wood',5),('sapling',6),
 ('adminium',7),('bedrock',7),('water',8),('stillwater',9),
@@ -99,10 +99,10 @@ INSERT INTO `items` VALUES
 ('sand',12),('gravel',13),('goldore',14),('ironore',15),
 ('coalore',16),('tree',17),('log',17),('leaves',18),
 ('sponge',19),('glass',20),('lapislazuliore',21),('lapislazuliblock',22),
-('dispenser',23),('sandstone',24),('noteblock',25),('poweredrail',27),
+('dispenser',23),('sandstone',24),('noteblock',25),('bedblock',26),('poweredrail',27),
 ('boosterrail',27),('detectorrail',28),('stickypiston',29),('cobweb',30),
-('tallgrass',31),('deadshrub',32),('piston',33),('cloth',35),
-('flower',37),('rose',38),('brownmushroom',39),('redmushroom',40),
+('tallgrass',31),('deadshrub',32),('piston',33),('pistonextension',34),('cloth',35),
+('pistonmovedblock',36),('flower',37),('rose',38),('brownmushroom',39),('redmushroom',40),
 ('gold',41),('goldblock',41),('iron',42),('ironblock',42),
 ('doublestair',43),('stair',44),('step',44),('brickblock',45),
 ('brickwall',45),('tnt',46),('bookshelf',47),('bookcase',47),
@@ -119,22 +119,25 @@ INSERT INTO `items` VALUES
 ('ice',79),('snowblock',80),('cactus',81),('clayblock',82),
 ('reedblock',83),('jukebox',84),('fence',85),('pumpkin',86),
 ('netherstone',87),('slowsand',88),('glowstone',89),('portal',90),
-('jackolantern',91),('jacko',91),('lockedchest',95),('trapdoor',96),
+('jackolantern',91),('jacko',91),('cakeblock',92),('repeateron',93),
+('repeateroff',94),('lockedchest',95),('trapdoor',96),
 ('silverblock',97),('stonebrick',98),('hugebrownmushroom',99),
 ('hugeredmushroom',100),('ironbars',101),('glasspane',102),('melonblock',103),
 ('pumpkinstem',104),('melonstem',105),('vine',106),('fencegate',107),
 ('brickstair',108),('stonebrickstair',109),('mycelium',110),('lilypad',111),
 ('netherbrick',112),('netherbrickfence',113),('netherbrickstairs',114),('netherwartblock',115),
-('enchantmenttable',116),('brewingstandblock',117),('cauldron',118),('endportal',119),
+('enchantmenttable',116),('brewingstandblock',117),('cauldronblock',118),('endportal',119),
 ('endportalframe',120),('endstone',121),('dragonegg',122),('redstonelampoff',123),('redstonelampon',124),
 ('woodendoubleslab',125),('woodenslab',126),('cocoaplant',127),('sandstonetairs',128),
-('emeraldore',129),('enderchest',130),('tripwirehook',131),('tripwire',  132),('emeraldblock',133),
-('sprucewoodstairs',  134),('birchwoodstairs',  135),('junglewoodstairs',  136),('commandblock',  137),
-('beacon',  138),('cobblestonewall',  139),('flowerpot',  140),('carrots',  141),('potatoes',  142),
-('woodenbutton',  143),('head',  144),('anvil',  145),('trapchest', 146),('weightedplatelight', 147),
-('weightedplateheavy', 148),('comparatorinactive', 149),('comparatoractive', 150),('daylightsensor', 151),
-('redstoneblock', 152),('netherquartzore', 153),('hopper',154),('blockofquartz', 155),('activatorrail', 157),
-('dropper', 158),('ironshovel',256),('ironspade',256),('ironpickaxe',257),('ironpick',257),('ironaxe',258),
+('emeraldore',129),('enderchest',130),('tripwirehook',131),('tripwire',132),('emeraldblock',133),
+('sprucewoodstairs',134),('birchwoodstairs',135),('junglewoodstairs',136),('commandblock',137),
+('beacon',138),('cobblestonewall',139),('flowerpot',140),('carrots',141),('potatoes',142),
+('woodenbutton',143),('head',144),('anvil',145),('trapchest', 146),('weightedplatelight',147),
+('weightedplateheavy',148),('comparatorinactive', 49),('comparatoractive',150),('daylightsensor',151),
+('redstoneblock',152),('netherquartzore',153),('hopper',154),
+('blockofquartz',155),('hopper',156),('activatorrail',157),('dropper',158),
+('stainedclay',159),('hayblock',170),('carpet',171),('hardenedclay',172),('coalblock',172),
+('ironshovel',256),('ironspade',256),('ironpickaxe',257),('ironpick',257),('ironaxe',258),
 ('flintandsteel',259),('lighter',259),('apple',260),('bow',261),
 ('arrow',262),('coal',263),('diamond',264),('ironbar',265),
 ('goldbar',266),('ironsword',267),('woodsword',268),('woodshovel',269),
@@ -169,16 +172,17 @@ INSERT INTO `items` VALUES
 ('rottenflesh',367),('enderpearl',368),('blazerod',369),('ghasttear',370),
 ('goltnugget',371),('netherwart',372),('potion',373),('glassbottle',374),
 ('spidereye',375),('fermentedspidereye',376),('blazepowder',377),('magmacream',378),
-('brewingstand',379),('eyeofender',381),('glisteringmelon',382),('spawnegg',383),
+('brewingstand',379),('cauldron',380),('eyeofender',381),('glisteringmelon',382),('spawnegg',383),
 ('bottleoenchanting',384),('firecharge',385),('bookandquill',386),('writtenbook',387),
-('emerald', 388),('itemframe',  389),('flowerpot',  390),('carrot',  391),('potato',  392),
-('bakedpotato',  393),('poisonouspotato',  394),('emptymap',  395),('goldencarrot',  396),
-('skull',  397),('head',  397),('carrotonastick',  398),('netherstar',  399),('pumpkinpie',  400),
-('firework',  401), ('fireworkstar',  402),('enchantedbook', 403),('redstonecomparator', 404),
-('netherbrick', 405),('netherquartz', 406),('minecartwithtnt', 407),('minecartwithhopper', 408),
+('emerald',388),('itemframe',389),('flowerpot',390),('carrot',391),('potato',392),
+('bakedpotato',393),('poisonouspotato',394),('emptymap',395),('goldencarrot',396),
+('skull',397),('head',397),('carrotonastick',398),('netherstar',399),('pumpkinpie',400),
+('firework',401), ('fireworkstar',402),('enchantedbook',403),('redstonecomparator',404),
+('netherbrick',405),('netherquartz',406),('minecartwithtnt',407),('minecartwithhopper',408),
+('ironhorsearmor',417),('goldhorsearmor',418),('diamondhorsearmor',419),('lead',420),('nametag',421),
 ('goldrecord',2256),('greenrecord',2257),('blocksrecord',2258),
 ('chirprecord',2259),('farrecord',2260),('mallrecord',2261),('mellohirecord',2262),
-('stalrecord',2263),('stradrecord',2264),('wardrecord',2265),('11record',2266);
+('stalrecord',2263),('stradrecord',2264),('wardrecord',2265),('11record',2266),('waitrecord',2267);
 /*!40000 ALTER TABLE `items` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -281,7 +285,7 @@ CREATE TABLE IF NOT EXISTS `antixrayblocks` (
 
 LOCK TABLES `antixrayblocks` WRITE;
 /*!40000 ALTER TABLE `antixrayblocks` DISABLE KEYS */;
-INSERT INTO `antixrayblocks` VALUES 
+INSERT INTO `antixrayblocks` VALUES
 (14),(15),(16),(21),(56),(73);
 /*!40000 ALTER TABLE `antixrayblocks` ENABLE KEYS */;
 UNLOCK TABLES;
@@ -298,7 +302,7 @@ CREATE TABLE IF NOT EXISTS `enderblocks` (
 
 LOCK TABLES `enderblocks` WRITE;
 /*!40000 ALTER TABLE `enderblocks` DISABLE KEYS */;
-INSERT INTO `enderblocks` VALUES 
+INSERT INTO `enderblocks` VALUES
 (1),(2),(3),(4),(5),(12),(13),(14),(15),(16),(17),
 (18),(19),(20),(21),(22),(24),(35),(37),(38),(39),
 (40),(41),(42),(45),(46),(47),(48),(56),(57),(58),
